312d88a683e1b2810edf0a298095c8006a3bb764
Tovmas <Tovmas>
2023-02-01 21:34:08 +0400

    newset fixxx

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config()
+dotenv.config({path:'/etc/secrets/.env'})
+
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
