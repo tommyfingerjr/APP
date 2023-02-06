d02fcb1c59aa2ed3958c1ec7a01f1a8dcb4b78f0
Tovmas <Tovmas>
2023-02-06 19:08:23 +0400

    idealno 2

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `<|endoftext|>`,
       temperature: 0.7, // Higher values means the model will take more risks.
       max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 0.9, // alternative to sampling with temperature, called nucleus sampling
